ed766c040a22cfc3502a1a5f698cb70f099f4a18
Chow Jing Lun <chowjl94@gmail.com>
2021-09-01 10:43:52 +0800

    Fix:changed host to localhost

--- html.js
+++ html.js
@@ -1,13 +1,18 @@
+// Sample server that serves direct HTML
 const http = require("http");
 
 const host = 'localhost';
 const port = 8000;
 
 const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
-    res.end("My first server!");
+    res.end(`<html><body><h1>This is HTML</h1></body></html>`);
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)});
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+//random sagja
