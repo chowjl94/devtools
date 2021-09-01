b5b4c75a377373e8e948681b43fad294bc765e5d
Chow Jing Lun <chowjl94@gmail.com>
2021-09-01 09:56:39 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+// sends process to node
+// process is a collection of commands
+// process id is tagged to a socket(port)
+// os takes note of processes by keeping track of process ids
